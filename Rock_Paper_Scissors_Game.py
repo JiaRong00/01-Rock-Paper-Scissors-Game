d8442da9fe55dadb5ff1e6e200ea3f250e916e00
JiaRong00 <149306287+JiaRong00@users.noreply.github.com>
2023-11-25 19:03:17 +0800

    Add files via upload

---
+++ Rock_Paper_Scissors_Game.py
@@ -0,0 +1,43 @@
+# This is the rock paper scissors game.
+
+#Initiate game
+game_continue=True
+import sys
+print("Welcome to the rock paper scissors game!")
+start_game=input("Type Yes if you would like to begin the game\n").lower()
+choices={1:"Rock", 2:"Scissors", 3:"Paper"}
+
+while game_continue:
+    if start_game =='yes':
+        playerchoice=int(input("Please enter:\n1 for Rock✊\n2 for Scissors✌️\n3 for Paper✋\n"))
+        if playerchoice != 1 and playerchoice!=2 and playerchoice !=3:
+            playerchoice=input("You have entered an invalid value. Please try again.\nPlease enter:\n1 for Rock✊\n2 for Scissors✌️\n3 for Paper✋\n")
+    #Computer chooses a random choice
+        import random
+        choice_list=[1,2,3]
+        computer_choice=random.choice(choice_list)
+    else:
+        sys.exit("Thank you for your time, please join us next time!")
+
+#Determinining who won. 
+# Rock (1) wins scissors (2) but lose to paper (3)
+# Scissors (2) wins paper (3)
+    print(f"You chose {choices[playerchoice]}\nComputer chose {choices[computer_choice]}")
+    if playerchoice==computer_choice:
+        print("There is a tie🙌")
+    elif playerchoice ==1 and computer_choice==2:
+        print("You win!😁")
+    elif playerchoice ==2 and computer_choice==3:
+        print("You win!😁")
+    elif playerchoice ==3 and computer_choice==1:
+        print("You win!😁")
+    else:
+        print("You lose!😭")
+    start_game=input("Would you like to play another round? Please enter Yes or No.\n").lower()
+    if start_game=="no":
+        game_continue=False
+        sys.exit("Thank you for playing!")
+
+
+
+
