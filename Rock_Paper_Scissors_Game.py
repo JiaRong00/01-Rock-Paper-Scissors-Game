29a63a7d790563182df0596f7bf62c5c4601887b
Jia Rong <149306287+JiaRong00@users.noreply.github.com>
2023-12-03 11:34:46 +0800

    Update Rock_Paper_Scissors_Game.py

--- Rock_Paper_Scissors_Game.py
+++ Rock_Paper_Scissors_Game.py
@@ -7,18 +7,20 @@ print("Welcome to the rock paper scissors game!")
 start_game=input("Type Yes if you would like to begin the game\n").lower()
 choices={1:"Rock", 2:"Scissors", 3:"Paper"}
 timesplayed=0
-losecount=0
+wincount=0
 
-#While loop to keep playing the game 
-while game_continue:
+#Define function to play game
+def game():
     if start_game =='yes':
         playerchoice=int(input("Please enter:\n1 for Rock✊\n2 for Scissors✌️\n3 for Paper✋\n"))
-        if playerchoice != 1 and playerchoice!=2 and playerchoice !=3:
-            playerchoice=input("You have entered an invalid value. Please try again.\nPlease enter:\n1 for Rock✊\n2 for Scissors✌️\n3 for Paper✋\n")
+        #while loop to prompt player to enter valid input 
+        while playerchoice != 1 and playerchoice!=2 and playerchoice !=3:
+            playerchoice=int(input("You have entered an invalid value. Please try again.\nPlease enter:\n1 for Rock✊\n2 for Scissors✌️\n3 for Paper✋\n"))
     #Computer chooses a random choice
         import random
         choice_list=[1,2,3]
         computer_choice=random.choice(choice_list)
+        global timesplayed
         timesplayed+=1
     else:
         sys.exit("Thank you for your time, please join us next time!")
@@ -26,25 +28,32 @@ while game_continue:
 #Determinining who won. 
 # Rock (1) wins scissors (2) but lose to paper (3)
 # Scissors (2) wins paper (3)
+    global wincount
     print(f"You chose {choices[playerchoice]}\nComputer chose {choices[computer_choice]}")
     if playerchoice==computer_choice:
         print("There is a tie🙌")
-        losecount+=1
     elif playerchoice ==1 and computer_choice==2:
         print("You win!😁")
+        wincount+=1
     elif playerchoice ==2 and computer_choice==3:
         print("You win!😁")
+        wincount+=1
     elif playerchoice ==3 and computer_choice==1:
         print("You win!😁")
+        wincount+=1
     else:
         print("You lose!😭")
-        losecount+=1
-    print(f"You have won {timesplayed-losecount} time(s) out of {timesplayed} round(s).")
+    print(f"You have won {wincount} time(s) out of {timesplayed} round(s).")
+    
+#While loop to continue playing game
+while True: 
+    game()  
     start_game=input("Would you like to play another round? Please enter Yes or No.\n").lower()
-    if start_game=="no":
-        game_continue=False
-        sys.exit("Thank you for playing!")
-
+    if start_game=="yes":
+        continue
+    else:
+        print("Thank you for playing!")
+        break
 
 
 
